562ad638a745e634ed0da1a671000623dc077aa8
Amit Rajbhar <amitrajbhar102@gmail.com>
2025-06-07 15:22:25 +0530

    third commit

--- src/com/git/command/tutorial/GitCommandTutorial.java
+++ src/com/git/command/tutorial/GitCommandTutorial.java
@@ -1,11 +1,12 @@
 package com.git.command.tutorial;
 
+import com.git.command.tutorial.data.GitCommand1;
+
 public class GitCommandTutorial {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git Command tutorial");
 		System.out.println("Welcome home");
-		
-		
-		
+		GitCommand1 gitCommand1=new GitCommand1();
+		gitCommand1.m1();
 	}
 }
---
+++ src/com/git/command/tutorial/data/GitCommand1.java
@@ -0,0 +1,8 @@
+package com.git.command.tutorial.data;
+
+public class GitCommand1 {
+
+	public void m1() {
+		System.out.println("Hello world");
+	}
+}
