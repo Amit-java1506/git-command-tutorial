62a3b1049206d0fea1c3202cca50543bd37b47b5
Amit Rajbhar <amitrajbhar102@gmail.com>
2025-06-07 16:22:44 +0530

    added Gitcommand2 file

--- src/com/git/command/tutorial/data/GitCommand1.java
+++ src/com/git/command/tutorial/data/GitCommand1.java
@@ -3,6 +3,6 @@ package com.git.command.tutorial.data;
 public class GitCommand1 {
 
 	public void m1() {
-		System.out.println("Hello world");
+		System.out.println("Hello world Gitcommand1");
 	}
 }
---
+++ src/com/git/command/tutorial/data/Gitcommand2.java
@@ -0,0 +1,8 @@
+package com.git.command.tutorial.data;
+
+public class Gitcommand2 {
+
+	public void() {
+		System.out.println("Hello world from Gitcommand2");
+	}
+}
